a7297db2475b4941405f4947bbc3e9485b60bca3
semanursokmen <126008451+semanursokmen@users.noreply.github.com>
2026-04-07 14:34:02 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,21 +1,21 @@
 -- Supermarket Sales Analysis Queries
 
 -- Total sales by product category
-SELECT "Product Line", SUM(total) as total_sales
-FROM supermarket
+SELECT "Product Line", SUM(Sales) as total_sales
+FROM sales
 GROUP BY "Product Line";
 
 -- Total sales by city
-SELECT "City", SUM(total) as total_sales
-FROM supermarket
+SELECT "City", SUM(Sales) as total_sales
+FROM sales
 GROUP BY "City";
 
 -- Total sales by gender
-SELECT "Gender", SUM(total) as total_sales
-FROM supermarket
+SELECT "Gender", SUM(Sales) as total_sales
+FROM sales
 GROUP BY "Gender";
 
 -- Average spending by gender
-SELECT "Gender", AVG(total) as avg_spending
-FROM supermarket
+SELECT "Gender", AVG(Sales) as avg_spending
+FROM sales
 GROUP BY "Gender";
